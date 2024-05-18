12e201c7da2b0ca39901ff03087b2511db4d3c26
Y-Hashish <emady2706@gmail.com>
2024-05-18 21:58:17 +0300

    home & confirm

--- Base_of_Project/lib/features/onboarding/controller/cubit/onboarding_cotroller_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'onboarding_cotroller_cubit.dart';
-
-// @immutable
- class OnboardingCotrollerState {}
-
- class OnboardingCotrollerInitial extends OnboardingCotrollerState {}
- class OnboardingPageChange extends OnboardingCotrollerState {}
